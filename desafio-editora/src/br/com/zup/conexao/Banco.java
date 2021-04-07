3836767bcf464a65d4eda0247891b552c941fa19
Guilherme Joaquim <Guilherme Joaquim@ZUP-4277.local>
2021-04-07 18:41:54 -0300

    Realizando o Primeiro Commit

---
+++ desafio-editora/src/br/com/zup/conexao/Banco.java
@@ -0,0 +1,14 @@
+package br.com.zup.conexao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.zup.model.ModelAutor;
+
+public class Banco {
+	
+	private static List<ModelAutor> listaAutores = new ArrayList<>();
+	private static Integer idSequencial = 1;
+	
+
+}
